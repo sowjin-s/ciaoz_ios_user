4417fbb8750dc79c37bd0daf39047057f6f0e312
apple <apple@apples-MacBook-Pro.local>
2019-01-21 18:11:26 +0530

    .gitignore is now working

--- Pods/Target Support Files/KWDrawerController/KWDrawerController-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_KWDrawerController : NSObject
-@end
-@implementation PodsDummy_KWDrawerController
-@end
--- Pods/Target Support Files/Pods-TranxitUser/Pods-TranxitUser-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_TranxitUser : NSObject
-@end
-@implementation PodsDummy_Pods_TranxitUser
-@end
