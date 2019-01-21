515d8436f79b1e10cde635165bb6a7eba5decaad
apple <apple@apples-MacBook-Pro.local>
2019-01-21 19:41:32 +0530

    Image picker library functionality implemented.

--- TranxitSwift/SelectImageView.swift
+++ TranxitSwift/SelectImageView.swift
@@ -6,7 +6,6 @@
 //
 
 import UIKit
-import Lightbox
 
 class SelectImageView : NSObject{
     
@@ -16,51 +15,30 @@ private var completion : (([UIImage])->())?
     
     func show(imagePickerIn view : UIViewController, completion : @escaping ([UIImage])->()){
         
-//        let config = Configuration()
-//        config.allowPinchToZoom = true
-//        config.canRotateCamera = true
-//        config.doneButtonTitle = Constants.string.Done
-//        config.flashButtonAlwaysHidden = false
-//        config.requestPermissionTitle = ""
-//        config.requestPermissionMessage = "will add later"
-//        self.completion = completion
-//        let imagePicker = ImagePickerController(configuration: config)
-//        imagePicker.imageLimit = 1
-//        imagePicker.delegate = self
-//        view.present(imagePicker, animated: true, completion: nil)
-        
+        let imagePicker = UIImagePickerController()
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.allowsEditing = true
+        imagePicker.delegate = self
+        view.present(imagePicker, animated: true, completion: nil)
     }
-    
     
 }
 
+// MARK: - UIImagePickerControllerDelegate
+extension SelectImageView: UIImagePickerControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let tempImage = info[UIImagePickerController.InfoKey.editedImage] as! UIImage
+        picker.dismiss(animated: true, completion: {
+            self.completion!([tempImage])
+        })
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+}
 
-//extension SelectImageView : ImagePickerDelegate {
-//
-//    // MARK: - ImagePickerDelegate
-//
-//    func cancelButtonDidPress(_ imagePicker: ImagePickerController) {
-//        imagePicker.dismiss(animated: true, completion: nil)
-//    }
-//
-//    func wrapperDidPress(_ imagePicker: ImagePickerController, images: [UIImage]) {
-//
-//        guard images.count > 0 else { return }
-//
-//        let lightboxImages = images.map {
-//            return LightboxImage(image: $0)
-//        }
-//
-//        let lightbox = LightboxController(images: lightboxImages, startIndex: 0)
-//        imagePicker.present(lightbox, animated: true, completion: nil)
-//
-//    }
-//
-//    func doneButtonDidPress(_ imagePicker: ImagePickerController, images: [UIImage]) {
-//
-//        imagePicker.dismiss(animated: true) {
-//            self.completion?(images)
-//        }
-//    }
-//
-//}
+// MARK: - UINavigationControllerDelegate
+extension SelectImageView: UINavigationControllerDelegate {
+    
+}
